aa015fe65413072767c465acfc0afb086ba8015f
Haidy <Haidy@MSI>
2024-06-21 00:54:25 +0300

    Add project files.

---
+++ Day02 G02/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Day02_G02
+{
+     class Program
+    {
+        //intry point 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            #region Comments
+            // Single line Comment
+            /**
+             * Multi
+             * line
+             *Comment 
+             */
+            #endregion
+
+            #region Varible Declaretions
+            int Id = 10;
+            //PascalCase = fristName
+            //camelCase = fristName
+            //Kabab-case = frist-name [Anglurs Files Naming ]
+            //snake_case = frist_name
+            Console.WriteLine(Id);
+
+            #endregion
+
+        }
+    }
+}
