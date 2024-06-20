0451441a22c2315c33a2fdd14274d500981eea27
Haidy08 <Haidi.sobhi.22@gmail.com>
2024-06-21 01:25:11 +0300

    Part 04 Variable Declaration and Comments

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -5,6 +5,25 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            #region Commments
+            // Single line Comment
+            /**
+             * Multi
+             * line
+             *Comment 
+             */ 
+            #endregion
+
+            #region Varible Declaretions
+            int Id = 10;
+            //PascalCase = fristName
+            //camelCase = fristName
+            //Kabab-case = frist-name [Anglurs Files Naming ]
+            //snake_case = frist_name
+            Console.WriteLine(Id);
+            #endregion
+
         }
     }
 }
