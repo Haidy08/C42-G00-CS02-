a3ba8295feea34000023a5573fda24ebc4e6c43c
Haidy08 <Haidi.sobhi.22@gmail.com>
2024-06-21 03:30:09 +0300

    Part 10 ValueType Vs ReferenceType [Code]

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Demo
+﻿using System.Drawing;
+
+namespace Demo
 {
     internal class Program
     {
@@ -31,7 +33,46 @@
             y = x;
             Console.WriteLine(y);
             x++;
-            Console.WriteLine(x); 
+            Console.WriteLine(x);
+            #endregion
+
+            #region CodeValue Type
+            int x;
+            // CLR will UnInitialized 4 Bytes at Stack 
+            //int : C# KeyWord
+            x = 5;
+
+            Int32 y = 10;
+            // CLR will UnInitialized 4 Bytes at Stack 
+            //int32 : BCL
+            y = x; //5
+            x++;  //6
+            Console.WriteLine(y);
+            Console.WriteLine(x);
+
+            #endregion
+
+            #region Reference Type
+
+            Point P1;
+            /// Declare for references of type 'point', references to Null
+            /// This References 'p1' can  refer to an object from type 'point' or anther type 'Inhereting from point
+            /// CLR will allocate 4 Byts for thr References at Stack
+            /// CLR will allocate 0 Byts for thr References atHeap
+            
+            P1 = new Point();
+            ///1.Allocate required Number of bytes in Heap
+            ///2.Initialized allocated of it Bytes with Defulte Value of its DataType
+            ///3.Call user Defined Constructor of Exsist
+            ///4.Assign the references with allocated object
+            Console.WriteLine(P1.X);
+            Console.WriteLine(P1.Y);
+
+            Point P2 = new Point();
+            P2 = P1;
+            P2.X = 10;
+
+            Console.WriteLine(P1.X);
             #endregion
         }
     }
